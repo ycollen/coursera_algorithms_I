d67a25b92a409544546659de00fae46feb858940
Yan <ycollen@gmail.com>
2017-08-27 11:53:00 +0100

    RandomizedQueue

--- src/dequeAndRandomizedQueues/RandomizedQueue.java
+++ src/dequeAndRandomizedQueues/RandomizedQueue.java
@@ -5,7 +5,7 @@ import edu.princeton.cs.algs4.StdRandom;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
 	public RandomizedQueue() {
-
+		array = (Item[]) new Object[capacity];
 	}
 
 	public boolean isEmpty() {
@@ -16,23 +16,19 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 		return nbElts;
 	}
 
-	public void enqueue(Item item) {
-		// initial capacity
-		if (this.isEmpty()) {
-			array = (Item[]) new Object[capacity];
+	public void enqueue(Item item) throws java.lang.IllegalArgumentException {
+		if (item == null) {
+			throw new java.lang.IllegalArgumentException("Trying to enqueue null item");
 		}
 		// check if full, resize
-		if (last == array.length) {
-			// if full, resize
-			if (nbElts == array.length) {
-				// resize to twice the current capacity
-				resize(capacity * 2);
+		if (last == capacity) {
+			// if there are holes, compact before resizing
+			if (nbElts < capacity) {
+				this.compact();
+			} else {
+				// if full, resize twice the current capacity
+				this.resize(capacity * 2);
 				capacity = capacity * 2;
-			}
-			// if not full, compact
-			// TODO possible optimization?
-			else if (nbElts < array.length) {
-				// compact
 			}
 		}
 		// add item at last position and increment last
@@ -40,26 +36,79 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 		this.nbElts++;
 	}
 
-	public Item dequeue() {
-		// choose a random item
-		int i = StdRandom.uniform(first, last);
-		return this.dequeue(i);
+	private void compact() {
+		Item[] tmp = (Item[]) new Object[capacity];
+		// iterate through from position 0 to last item
+		// pointer to next element after last in tmp array
+		int j = 0;
+		for (int i = 0; i < last; i++) {
+			// if an item is present copy it
+			if (array[i] != null) {
+				tmp[j] = array[i];
+				j++;
+			}
+		}
+		array = tmp;
+		// update last pointer
+		last = j;
 	}
 
-	public Item dequeue(int i) {
+	public Item dequeue() throws java.util.NoSuchElementException {
+		if (this.isEmpty()) {
+			throw new java.util.NoSuchElementException("Trying to dequeue from an empty queue");
+		}
+		int i = StdRandom.uniform(last);
+		while (array[i] == null) {
+			i = StdRandom.uniform(last);
+		}
+		return dequeue(i);
+	}
+
+	private Item dequeue(int i) {
+		System.out.println("dequeuing " + i);
+		System.out.println("capacity " + capacity);
+		System.out.println("nbElts " + this.size());
 		Item item = array[i];
 		array[i] = null;
-		if (first == i) {
-			first++;
-		}
+		nbElts--;
+		// if last item of queue was removed, update last pointer item
 		if (last == i + 1) {
 			last--;
+			// if previous item is null continue to go back until a non null item is reached
+			while (last != 0 && array[last] == null) {
+				last--;
+			}
+		}
+		// shrink if quarter empty
+		if (!this.isEmpty() && capacity / nbElts > 3) {
+			// shrink
+			System.out.println("shrinking...");
+			Item[] tmp = (Item[]) new Object[capacity / 4];
+			// point to after last item in tmp
+			int j = 0;
+			for (int k = 0; k < capacity; k++) {
+				if (array[k] != null) {
+					tmp[j] = array[k];
+					j++;
+				}
+			}
+			array = tmp;
+			last = j;
+			capacity = capacity / 4;
 		}
 		return item;
 	}
 
-	public Item sample() {
-		return null;
+	public Item sample() throws java.util.NoSuchElementException {
+		if (this.isEmpty()) {
+			throw new java.util.NoSuchElementException("Trying to sample from empty queue");
+		}
+		int i = StdRandom.uniform(last);
+
+		while (array[i] == null) {
+			i = StdRandom.uniform(0, last);
+		}
+		return array[i];
 	}
 
 	private void resize(int newCapacity) {
@@ -74,52 +123,50 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 	}
 
 	public Iterator<Item> iterator() {
-		return new RandomizedQueueIterator();
+		return new RandomizedQueueIterator(this);
 	}
 
 	class RandomizedQueueIterator implements Iterator<Item> {
+
+		public RandomizedQueueIterator(RandomizedQueue<Item> randomizedQueue) {
+			iteratorArray = (Item[]) new Object[randomizedQueue.size()];
+			RandomizedQueue<Item> copy = new RandomizedQueue();
+			current = 0;
+			for (int i = 0; i < randomizedQueue.capacity; i++) {
+				if (randomizedQueue.array[i] != null) {
+					copy.enqueue(array[i]);
+				}
+			}
+			// fill iterator array by dequeuing the copy of randomized array
+			int k = 0;
+			while (copy.isEmpty() == false) {
+				this.iteratorArray[k] = copy.dequeue();
+				k++;
+			}
+			System.out.println("finished building iterator");
+		}
+
 		public boolean hasNext() {
-			return true;
+			return current != iteratorArray.length;
 		}
 
-		public Item next() {
-			return null;
+		public Item next() throws java.util.NoSuchElementException {
+			if (hasNext()) {
+				return iteratorArray[current++];
+			} else {
+				throw new java.util.NoSuchElementException("Iterator does not have next items");
+			}
+
 		}
 
-	}
+		private Item[] iteratorArray;
+		private int current;
 
-	/**
-	 * @return the first
-	 */
-	public int getFirst() {
-		return first;
-	}
-
-	/**
-	 * @return the last
-	 */
-	public int getLast() {
-		return last;
-	}
-
-	/**
-	 * @return the capacity
-	 */
-	public int getCapacity() {
-		return capacity;
-	}
-
-	/**
-	 * @return the nbElts
-	 */
-	public int getNbElts() {
-		return nbElts;
 	}
 
 	private int last = 0; // pointing just after last item of the queue
 	private int nbElts = 0; // nb of elements in the queue
 	private int capacity = 2;
-	private int first = 0; // first item in the queue
 	private Item[] array;
 
 }
--- src/dequeAndRandomizedQueues/RandomizedQueueTest.java
+++ src/dequeAndRandomizedQueues/RandomizedQueueTest.java
@@ -2,10 +2,12 @@ package dequeAndRandomizedQueues;
 
 import static org.junit.Assert.*;
 
+import java.io.Console;
+import java.util.Iterator;
+
 import org.junit.Test;
 
 public class RandomizedQueueTest {
-
 
 	@Test
 	public void testIsEmpty() {
@@ -16,41 +18,106 @@ public class RandomizedQueueTest {
 		assertEquals("not empty", false, q.isEmpty());
 	}
 
-//	@Test
-//	public void testSize() {
-//		//fail("Not yet implemented");
-//	}
-//
 	@Test
 	public void testEnqueue() {
 		RandomizedQueue<String> q = new RandomizedQueue<String>();
 		q.enqueue("Item 1");
-		assertEquals("capacity = 2", 2, q.getCapacity());
-		assertEquals("first = 0", 0, q.getFirst());
-		assertEquals("last = 1", 1, q.getLast());
+		assertEquals("size is 1", 1, q.size());
 		q.enqueue("Item 2");
-		assertEquals("capacity = 2", 2, q.getCapacity());
-		assertEquals("first = 0", 0, q.getFirst());
-		assertEquals("last=2", 2, q.getLast());
+		assertEquals("size is 2", 2, q.size());
 		q.enqueue("Item 3");
-		assertEquals("capacity = 4", 4, q.getCapacity());
-		assertEquals("first = 0", 0, q.getFirst());
-		assertEquals("last = 3", 3, q.getLast());
+		assertEquals("size is 3", 3, q.size());
+
 	}
-//
-//	@Test
-//	public void testDequeue() {
-//		//fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testSample() {
-//		//fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testIterator() {
-//		//fail("Not yet implemented");
-//	}
+
+	@Test
+	public void testDequeue() {
+		RandomizedQueue<Integer> q = new RandomizedQueue<Integer>();
+		int max = 64;
+		for (int i = 0; i < max; i++) {
+			q.enqueue(i);
+		}
+		int j = max;
+		int i = 1;
+		while (q.isEmpty() == false) {
+			System.out.println("successfully dequeued " + i + " " + q.dequeue());
+			System.out.println("size is now " + q.size());
+			j--;
+			i++;
+			System.out.println("j = " + j);
+			System.out.println("Is empty? " + q.isEmpty());
+			assertEquals("size ", j, q.size());
+		}
+		assertEquals("empty", true, q.isEmpty());
+	}
+
+	@Test
+	public void testIterator() {
+
+		RandomizedQueue<Integer> q = new RandomizedQueue<Integer>();
+		int max = 64;
+		for (int i = 0; i < max; i++) {
+			q.enqueue(i);
+		}
+		Iterator<Integer> it = q.iterator();
+		int i = 0;
+		while (it.hasNext()) {
+			i++;
+			Integer itValue = it.next();
+			assertNotNull("should not be null", itValue);
+			System.out.println("iterator value " + itValue);
+		}
+		assertEquals("Iterator should have iterated 64 times", 64, i);
+	}
+
+	@Test(expected = java.lang.IllegalArgumentException.class)
+	public void enqueueNullItemException() {
+		RandomizedQueue<Integer> q = new RandomizedQueue();
+		q.enqueue(null);
+	}
+
+	@Test(expected = java.util.NoSuchElementException.class)
+	public void sampleFromEmptyQueue() {
+		RandomizedQueue<Integer> q = new RandomizedQueue();
+		q.sample();
+	}
+
+	@Test(expected = java.util.NoSuchElementException.class)
+	public void dequeueFromEmptyQueue() {
+		RandomizedQueue<Integer> q = new RandomizedQueue();
+		q.dequeue();
+	}
+
+	@Test(expected = java.lang.UnsupportedOperationException.class)
+	public void testRemoveFromIterator() {
+
+		RandomizedQueue<Integer> q = new RandomizedQueue<Integer>();
+		int max = 64;
+		for (int i = 0; i < max; i++) {
+			q.enqueue(i);
+		}
+		Iterator<Integer> it = q.iterator();
+		it.remove();
+
+	}
+
+	@Test(expected = java.util.NoSuchElementException.class)
+	public void testNextOnEmptyIterator() {
+
+		RandomizedQueue<Integer> q = new RandomizedQueue<Integer>();
+		int max = 64;
+		for (int i = 0; i < max; i++) {
+			q.enqueue(i);
+		}
+		Iterator<Integer> it = q.iterator();
+		int i = 0;
+		while (it.hasNext()) {
+			i++;
+			Integer itValue = it.next();
+			assertNotNull("should not be null", itValue);
+			System.out.println("iterator value " + itValue);
+		}
+		it.next();
+	}
 
 }
