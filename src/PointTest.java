e23f65b6b91a66979cccd78b6c6a4c2f14990e25
Yan <ycollen@gmail.com>
2017-09-18 17:50:34 +0100

    completed unit tests for compareTo

--- src/PointTest.java
+++ src/PointTest.java
@@ -7,7 +7,13 @@ public class PointTest {
 	@Test
 	public void testCompareTo() {
 		Point point = new Point(2,1);
-		assertEquals("(2,1) is smaller than (1,2)", -1, point.compareTo(new Point(1, 2)));
+		assertEquals("(2,1) is smaller than (2,2)", -1, point.compareTo(new Point(1, 2)));
+		assertEquals("(2,1) is equal to (2,1)", 0, point.compareTo(new Point(2,1)));
+		assertEquals("(2,1) is smaller than (3,1)", -1, point.compareTo(new Point(3,1)));
+		assertEquals("(2,1) is smaller than (0,4)", -1, point.compareTo(new Point(0,4)));
+		assertEquals("(2,1) is greater than (2,0)", 1, point.compareTo(new Point(2,0)));
+		assertEquals("(2,1) is greater than (1,0)", 1, point.compareTo(new Point(1,0)));
+		assertEquals("(2,1) is greater than (1,1)", 1, point.compareTo(new Point(1,1)));
 	}
 
 	@Test
